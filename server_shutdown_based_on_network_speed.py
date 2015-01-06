ca75cab377d153ae26b1c7eb2be322f5ea51a21c
Osess <osess.li@gmail.com>
2015-01-06 13:56:05 +0800

    Update server_shutdown_based_on_network_speed.py

--- server_shutdown_based_on_network_speed.py
+++ server_shutdown_based_on_network_speed.py
@@ -1,17 +1,16 @@
-## Outbound Traffic Monitor v0.1 - Jan 2014
-## Shutdown the server if the maximum outbound speed is reached.
+## Download Monitor v0.1 - March 2012
 
 # Set the interface you wish to monitor, eg: eth0, wlan0, usb0
 INTERFACE = "eth0"
 
-# Set the maximum out speed in KB/s, shutdown the server if reached.
-MAXIMUM_SPEED = 15
+# Set the minimum download speed in KB/s that must be achieved.
+MAXIMUM_SPEED = 2500 #2.5 MB/s or 20Mb/s
 
-# Set the number of retries to test for the average maximum speed. If the average speed is greater
-# than the maximum speed for x number of retries, then shutdown.
-RETRIES = 5
+# Set the number of retries to test for the average minimum speed. If the average speed is less
+# than the minimum speed for x number of retries, then shutdown.
+RETRIES = 3
 
-# Set the interval (in seconds), between retries to test for the maximum speed.
+# Set the interval (in seconds), between retries to test for the minimum speed.
 INTERVAL = 5
 
 
@@ -22,9 +21,9 @@ def worker ():
     RETRIES_COUNT = RETRIES
     while True:
         SPEED = int(float(getoutput("ifstat -i %s 1 1 | awk '{print $1}' | sed -n '3p'" % INTERFACE)))
-        if (SPEED < MAXIMUM_SPEED and RETRIES_COUNT <= 0):
+        if (SPEED > MAXIMUM_SPEED and RETRIES_COUNT <= 0):
             os.system("shutdown -h now")
-        elif SPEED < MINIMUM_SPEED:
+        elif SPEED > MAXIMUM_SPEED:
             RETRIES_COUNT -= 1
             time.sleep(INTERVAL)
         else:
